83ecd2f5ac3fba2955cbd305eb35da8f5169a8da
git-test <patvishwajeet123@gmail.com>
2024-05-30 16:01:22 +0530

    software

--- components/works/ReactBD.tsx
+++ components/works/ReactBD.tsx
@@ -10,7 +10,7 @@ const ReactBD = () => {
       className="w-full"
     >
       <h3 className="flex gap-1 font-medium text-xl font-titleFont">
-        TECHNICAL SUPPORT SPECIALIST
+        SOFTWARE ENGINEER
         <span className="text-textGreen tracking-wide">@eSSL</span>
       </h3>
       <p className="text-sm mt-1 font-medium text-textDark">
